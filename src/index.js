c27b26ee462018da8f3e6e8f94696c8970060866
Jelte De Proft <jelte.deproft@intix.eu>
2021-01-27 21:25:03 +0100

    trying to make constants

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ require("dotenv").config();
 const discord = require("discord.js");
 const bot = new discord.Client();
 
-bot.login(process.env.DISCORD_TOKEN.trim())
-    //     .then(() => bot.guilds.cache.first().fetch())
-    //     .then(guild => guild.channels.cache.find(channel => channel.id === process.env.DISCORD_CHANNEL.trim()).fetch())
-    //     .then(channel => {
-    //         channel.send("gathered ideas :\n").then((msg) => msg.pin());
-    //     });
+bot.login(process.env.DISCORD_TOKEN.trim());
+
+console.log("bot.guilds = " + bot.guilds.fetch.cache);
+const guild = bot.guilds.fetch.cache.first().fetch();
+const channel = ideasGuild.channels.cache.find(channel => channel.id === process.env.DISCORD_CHANNEL.trim()).fetch();
+const message = channel.fetchMessage(process.env.MESSAGE_ID);
+
 
 bot.on('message', msg => {
     if (msg.content.startsWith('!addIdea:')) {
         msg.reply('adding idea to sticky');
         console.log("searching channel");
-        guild => guild.channels.cache.find(channel => channel.id === process.env.DISCORD_CHANNEL.trim()).fetch().fetchMessage(process.env.MESSAGE_ID).then(stickyMessage => {
-            console.log("channel found : " + channel);
-            console.log("stickyMessage found : " + stickyMessage);
-            stickyMessage.edit(stickyMessage.content + "\n" + msg.content.substring(9, msg.content.length - 1));
-        });
+        console.log("guild = " + guild);
+        console.log("channel = " + channel);
+        console.log("message = " + message);
+        message.edit(message.content + "\n" + msg.content.substring(9, msg.content.length - 1));
     }
 });
